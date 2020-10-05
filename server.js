f43cd402c4ea506c062a077b3af2af8c6fc3dfd2
Janine Lapiz <jglapiz@up.edu.ph>
2020-10-06 04:48:31 +0800

    Added minified version of app

---
+++ server.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const app = express();
+
+app.use(express.static(`${__dirname}/build`));
+app.get('/', (req, res) => 
+	res.sendFile(`${__dirname}/build/index.html`)
+	);
+
+app.listen(3000, () => console.log('Server started on port 3000'));
