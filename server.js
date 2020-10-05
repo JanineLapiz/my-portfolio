5752f4494479a84bdc2ca646c6c3355d1be2aaff
Janine Lapiz <jglapiz@up.edu.ph>
2020-10-06 05:13:42 +0800

    Modified port value

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const app = express();
+const port = process.env.PORT || 3000;
 
 app.use(express.static(`${__dirname}/build`));
 app.get('/', (req, res) => 
 	res.sendFile(`${__dirname}/build/index.html`)
 	);
 
-app.listen(3000, () => console.log('Server started on port 3000'));
+app.listen(port, () => console.log('Server started on port ${port}'));
