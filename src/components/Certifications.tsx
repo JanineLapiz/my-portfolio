5331ae802eef971cc406404006f38392bcfac96c
JanineLapiz <janine@restoplus.com.au>
2025-07-07 20:45:02 +0800

    Refactor certifications

---
+++ src/components/Certifications.tsx
@@ -0,0 +1,25 @@
+import { certifications } from '../data/certifications';
+import { Certification } from '../models/Certification';
+import { goToUrl } from '../utils/goToUrl';
+import { Card } from '../widgets/Card';
+import { Section } from '../widgets/Section';
+
+export const Certifications = (): JSX.Element => {
+  return (
+    <Section title="Certifications">
+      {certifications.map((certification) => (
+        <Certification {...certification} />
+      ))}
+    </Section>
+  );
+};
+
+const Certification = (certification: Certification): JSX.Element => {
+  return (
+    <Card title={certification.title} onClick={() => certification.url && goToUrl(certification.url)}>
+      <div className="issued-by">Issued By: {certification.issuedBy}</div>
+      <div className="issued-date">Issued Date:{certification.issuedDate}</div>
+      <div className="expiry">Expiry: {certification.expiresDate}</div>
+    </Card>
+  );
+};
