8b2e0aec3c2a95a567e82278233b345fab45ad5e
JanineLapiz <janine@restoplus.com.au>
2025-07-07 22:36:17 +0800

    Remove trailing commas

--- src/data/profiles.tsx
+++ src/data/profiles.tsx
@@ -1,31 +1,35 @@
 import { FaCodepen, FaFreeCodeCamp, FaGithub, FaLinkedin } from 'react-icons/fa';
-import { Profile, ProfileProvider } from '../models/Profile';
-import { SiIndeed } from 'react-icons/si';
+import { Profile, ProfileHost } from '../models/Profile';
+import { SiGmail, SiIndeed } from 'react-icons/si';
 
-export const profiles = (iconSize: number | string): Profile[] => [
+export type ProfileConfig = {
+  iconSizePx: number;
+};
+
+export const profiles: Profile[] = [
   {
-    icon: <FaCodepen {...{ size: iconSize }} />,
-    provider: ProfileProvider.CODEPEN,
-    url: 'https://github.com/BlueberryFridge',
+    icon: <FaCodepen />,
+    host: ProfileHost.CODEPEN,
+    url: 'https://github.com/BlueberryFridge'
   },
   {
-    icon: <FaFreeCodeCamp {...{ size: iconSize }} />,
-    provider: ProfileProvider.FREE_CODE_CAMP,
-    url: 'https://www.freecodecamp.org/blueberryfridge',
+    icon: <FaFreeCodeCamp />,
+    host: ProfileHost.FREE_CODE_CAMP,
+    url: 'https://www.freecodecamp.org/blueberryfridge'
   },
   {
-    icon: <FaGithub {...{ size: iconSize }} />,
-    provider: ProfileProvider.GITHUB,
-    url: 'https://github.com/JanineLapiz',
+    icon: <FaGithub />,
+    host: ProfileHost.GITHUB,
+    url: 'https://github.com/JanineLapiz'
   },
   {
-    icon: <SiIndeed {...{ size: iconSize }} />,
-    provider: ProfileProvider.INDEED,
-    url: 'https://my.indeed.com/p/janinel-r17eh31',
+    icon: <SiIndeed />,
+    host: ProfileHost.INDEED,
+    url: 'https://my.indeed.com/p/janinel-r17eh31'
   },
   {
-    icon: <FaLinkedin {...{ size: iconSize }} />,
-    provider: ProfileProvider.FREE_CODE_CAMP,
-    url: 'https://www.linkedin.com/in/janinelapiz/',
-  },
+    icon: <FaLinkedin />,
+    host: ProfileHost.FREE_CODE_CAMP,
+    url: 'https://www.linkedin.com/in/janinelapiz/'
+  }
 ];
--- src/data/projects.ts
+++ src/data/projects.ts
@@ -5,44 +5,44 @@ export const projects: Project[] = [
     id: '1',
     title: 'Pomodoro Clock',
     repoUrl: 'https://codepen.io/BlueberryFridge/pen/gOrowYZ',
-    iconUrl: './images/pomodoro-clock.png',
+    iconUrl: './images/pomodoro-clock.png'
   },
   {
     id: '2',
     title: 'Markdown Editor and Previewer',
     repoUrl: 'https://codepen.io/BlueberryFridge/pen/eYZdaEK',
-    iconUrl: './images/md-editor.png',
+    iconUrl: './images/md-editor.png'
   },
   {
     id: '3',
     title: 'JavaScript Calculator',
     repoUrl: 'https://codepen.io/BlueberryFridge/pen/RwaVeMe',
-    iconUrl: './images/js-calc.png',
+    iconUrl: './images/js-calc.png'
   },
   {
     id: '4',
     title: 'Drum Machine',
     repoUrl: 'https://codepen.io/BlueberryFridge/pen/MWybVyz',
-    iconUrl: './images/drum-machine.png',
+    iconUrl: './images/drum-machine.png'
   },
   {
     id: '5',
     title: 'Random Quote Generator',
     repoUrl: 'https://codepen.io/BlueberryFridge/pen/BaKomWy',
-    iconUrl: './images/quote-gen.png',
+    iconUrl: './images/quote-gen.png'
   },
   {
     id: '6',
     title: 'Inventory App',
     clientUrl: 'https://shielded-ridge-27315.herokuapp.com/inventory',
     repoUrl: 'https://github.com/BlueberryFridge/inventory-app',
-    iconUrl: './images/inventory-app.png',
+    iconUrl: './images/inventory-app.png'
   },
   {
     id: '7',
     title: 'My Portfolio',
     clientUrl: 'https://damp-badlands-08236.herokuapp.com/',
     repoUrl: 'https://github.com/BlueberryFridge/my-portfolio',
-    iconUrl: './images/my-portfolio.png',
-  },
+    iconUrl: './images/my-portfolio.png'
+  }
 ];
